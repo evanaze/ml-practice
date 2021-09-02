b053494127012f932877359092621bb237b958f3
Evan Azevedo <evan@azevedo.com>
2021-09-02 07:15:39 -0700

    moving knn to models dir

--- models/knn/test.py
+++ models/knn/test.py
@@ -4,7 +4,7 @@ from solution import predict
 
 
 def load_data():
-    return pd.read_csv("../data/reg_train.csv"), pd.read_csv("../data/reg_test.csv")
+    return pd.read_csv("./data/reg_train.csv"), pd.read_csv("./data/reg_test.csv")
 
 def test_solution(train, test):
     assert(round(math.sqrt(((test["OUTCOME"] - predict(train, test, k=5))**2).mean()), 7) == 0.3638982)
